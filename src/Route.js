62c0e3dd030c485c1b891ea423b6b9c157d11da7
Ranie Santos <ranietots32@gmail.com>
2018-01-01 17:21:30 +0800

    Major refactor of Route class

--- src/Route.js
+++ src/Route.js
@@ -4,35 +4,72 @@ export default class Route {
     constructor (path) {
         this.path = fixSlashes(path);
         this._guards = [];
+        this._setters = {
+            beforeEnter (guard) {
+                if (Array.isArray(guard)) {
+                    this._guards = this._guards.concat(guard);
+                } else {
+                    this._guards.push(guard);
+                }
+
+                this.beforeEnter = (to, from, next) => {
+                    const destination = window.location.href;
+                    this._guards.forEach((guard) => {
+                        const redirected = (window.location.href !== destination);
+                        if (!redirected) {
+                            guard(to, from, next);
+                        }
+                    });
+                };
+            },
+            prefix (prefix) {
+                this.path = fixSlashes(prefix + this.path);
+            }
+        };
     }
 
     options (options) {
-        const keys = {
-            default: [
-                'name',
-                'components',
-                'redirect',
-                'props',
-                'alias',
-                'children',
-                'beforeEnter',
-                'meta',
-                'caseSensitive',
-                'pathToRegexpOptions'
-            ],
-            custom: ['as', 'guard', 'prefix']
+        const valid = [
+            'name',
+            'components',
+            'redirect',
+            'props',
+            'alias',
+            'children',
+            'beforeEnter',
+            'meta',
+            'caseSensitive',
+            'pathToRegexpOptions',
+            'as',
+            'guard',
+            'prefix'
+        ];
+        const aliases = {
+            as: 'name',
+            guard: 'beforeEnter'
         };
-        const handler = (options, keys, callback) => {
-            const paths = ['redirect', 'alias', 'prefix'];
-            keys.forEach((key) => {
-                if (options.hasOwnProperty(key)) {
-                    const value = (paths.includes(key) ? fixSlashes(options[key]) : options[key]);
-                    callback(key, value);
+        const paths = ['redirect', 'alias', 'prefix'];
+
+        valid.forEach((key) => {
+            let value = options[key];
+            if (options.hasOwnProperty(key)) {
+                // swap alias
+                if (Object.keys(aliases).includes(key)) {
+                    options[aliases[key]] = value;
+                    delete options[key];
                 }
-            });
-        };
-        handler(options, keys.custom, (key, value) => { this[key](value); });
-        handler(options, keys.default, (key, value) => { this[key] = value; });
+                // format paths
+                if (paths.includes(key)) {
+                    value = fixSlashes(value);
+                }
+                // set
+                if (this._setters.hasOwnProperty(key)) {
+                    this._setters[key](value);
+                } else {
+                    this[key] = value;
+                }
+            }
+        });
         return this;
     }
 
@@ -42,27 +79,7 @@ export default class Route {
     }
 
     guard (guard) {
-        if (Array.isArray(guard)) {
-            this._guards = this._guards.concat(guard);
-        } else {
-            this._guards.push(guard);
-        }
-
-        this.beforeEnter = (to, from, next) => {
-            const destination = window.location.href;
-            this._guards.forEach((guard) => {
-                const redirected = (window.location.href !== destination);
-                if (!redirected) {
-                    guard(to, from, next);
-                }
-            });
-        };
-
-        return this;
-    }
-
-    prefix (prefix) {
-        this.path = fixSlashes(prefix + this.path);
+        this._setters.beforeEnter(guard);
         return this;
     }
 }
