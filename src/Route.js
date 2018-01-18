f3c5cf05e3f78fb42751612fef3165b96ee9d3a3
Ranie Santos <ranietots32@gmail.com>
2018-01-18 19:23:24 +0800

    Identifier name changes and refactoring

--- src/Route.js
+++ src/Route.js
@@ -1,35 +1,16 @@
-import { fixSlashes } from './util';
+import { fixSlashes, multiguard } from './util';
 
 export default class Route {
     constructor (path) {
-        this.path = fixSlashes(path);
+        this.config = { path: fixSlashes(path) };
         this._guards = [];
-    }
-
-    _set (key, value) {
-        const method = '_' + key;
-        if (this[method]) {
-            this[method](value);
-        } else {
-            this[key] = value;
-        }
     }
 
     options (options) {
         const valid = [
-            'name',
-            'components',
-            'redirect',
-            'props',
-            'alias',
-            'children',
-            'beforeEnter',
-            'meta',
-            'caseSensitive',
-            'pathToRegexpOptions',
-            'as',
-            'guard',
-            'prefix'
+            'name', 'components', 'redirect', 'props', 'alias',
+            'children', 'beforeEnter', 'meta', 'caseSensitive',
+            'pathToRegexpOptions', 'as', 'guard', 'prefix'
         ];
         const aliases = {
             as: 'name',
@@ -52,26 +33,8 @@ export default class Route {
         return this;
     }
 
-    _beforeEnter (guard) {
-        if (Array.isArray(guard)) {
-            this._guards = this._guards.concat(guard);
-        } else {
-            this._guards.push(guard);
-        }
-
-        this.beforeEnter = (to, from, next) => {
-            const destination = window.location.href;
-            this._guards.forEach((guard) => {
-                const redirected = (window.location.href !== destination);
-                if (!redirected) {
-                    guard(to, from, next);
-                }
-            });
-        };
-    }
-
     as (name) {
-        this.name = name;
+        this._set('name', name);
         return this;
     }
 
@@ -80,7 +43,22 @@ export default class Route {
         return this;
     }
 
+    _set (key, value) {
+        const method = '_' + key;
+        if (this[method]) {
+            this[method](value);
+        } else {
+            this.config[key] = value;
+        }
+    }
+
+    _beforeEnter (guard) {
+        guard = (Array.isArray(guard) ? guard : [guard]);
+        this._guards = this._guards.concat(guard);
+        this.config.beforeEnter = multiguard(this._guards);
+    }
+
     _prefix (prefix) {
-        this.path = fixSlashes(prefix + this.path);
+        this.config.path = fixSlashes(prefix + this.config.path);
     }
 }
