da83a53b9938a801d1448a0e26e7ff75519aac20
Ranie Santos <ranietots32@gmail.com>
2018-03-04 16:31:15 +0800

    Added support for nested routes, removed 'as' alias

--- src/util.js
+++ src/util.js
@@ -1,9 +1,11 @@
+import shared from './shared';
+
 export const fixSlashes = (path) => {
     if (!['/', '*'].includes(path)) {
         if (path.endsWith('/')) {
             path = path.substring(0, path.length - 1);
         }
-        if (!path.startsWith('/')) {
+        if (!path.startsWith('/') && shared.root) {
             path = '/' + path;
         }
     }
