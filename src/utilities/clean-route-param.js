9a14c5f9a7c43d72d363c090f6697bd127f4fbf9
Mike Rockett <mrockett@outlook.com>
2020-08-15 17:38:27 +0200

    wip: readme + changelog; feature: add support for aliased expression constraints

--- src/utilities/clean-route-param.js
+++ src/utilities/clean-route-param.js
@@ -2,6 +2,8 @@ export function cleanRouteParam(string) {
   return string
     .replace(/\}\{/, '}/{')
     .replace(/\{all\}/, '(.*)')
+    .replace(/\(number\)/, '(\\d+)')
+    .replace(/\(string\)/, '(\\w+)')
     .replace(/\{(\w+)\}/, ':$1')
     .trim()
 }
